6a3a458ed2ae55227d335a6c08215117635a360e
Chase Condron <chasecondron@gmail.com>
2023-02-07 13:16:56 -0500

    added .eslintrc.json

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const config = require('./config.json');
 const token = config.token;
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds]});
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
 const commands = [];
 
 client.commands = new Collection();
 
-const commandsPath = path.join(__dirname, '/src/commands')
+const commandsPath = path.join(__dirname, '/src/commands');
 const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
 for (const file of commandFiles) {
 	const filePath = path.join(commandsPath, file);
@@ -25,10 +25,11 @@ for (const file of eventsFiles) {
 	const filePath = path.join(eventsPath, file);
 	const event = require(filePath);
 	if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, commands))
-    } else {
+        client.once(event.name, (...args) => event.execute(...args, commands));
+    }
+	else {
         client.on(event.name, (...args) => event.execute(...args, commands));
     }
 }
-	
+
 client.login(token);
--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,16 +1,17 @@
 module.exports = {
-    name: "interactionCreate",
+    name: 'interactionCreate',
     once: false,
     async execute(interaction) {
         if (!interaction.isCommand()) return;
         const command = interaction.client.commands.get(interaction.commandName);
 
-        if(!command) {
+        if (!command) {
             console.error(`No command matching ${interaction.commandName} was found.`);
         }
         try {
-            await command.execute(interaction)
-        } catch (error) {
+            await command.execute(interaction);
+        }
+        catch (error) {
             console.error(`Error executing ${interaction.commandName}`);
             console.error(error);
         }
