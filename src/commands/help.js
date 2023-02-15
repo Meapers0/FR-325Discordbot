92ced3374ffa33ab48dd83f68ffccf9723c934b3
Chase Condron <chasecondron@gmail.com>
2023-02-14 21:40:02 -0500

    Fixed help.js

--- src/commands/help.js
+++ src/commands/help.js
@@ -5,10 +5,10 @@ module.exports = {
 		.setName('help')
 		.setDescription('A list of commands'),
 	async execute(interaction) {
-		console.log(interaction.client.commandNames);
+		console.log(interaction.client.commandData);
 		const embed = new EmbedBuilder()
 			.addFields(
-				interaction.client.commandNames);
+				interaction.client.commandData);
 		interaction.reply({ content: 'Help!', ephemeral: true, embeds: [embed] });
 	},
 };
