376b0f25d3029acff09881f2220d84a18f8b7fc0
Chase Condron <chasecondron@gmail.com>
2023-02-17 18:09:38 -0500

    Fixed merge

--- src/commands/role-sort.js
+++ src/commands/role-sort.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
-        .setName('reaction-message')
+        .setName('-message')
         .setDescription('Sets up the reaction role message')
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 };
