eefc29abdd977f64b5b3264094ee5ad388b85f9c
Chase Condron <chasecondron@gmail.com>
2023-02-21 13:28:36 -0500

    Changed interactionCreate.js

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -4,25 +4,26 @@ module.exports = {
     async execute(interaction) {
         if (!interaction.isCommand() || !interaction.isStringSelectMenu() || !interaction.isContextMenu()) {
             console.log(`Command Interaction ran. "${interaction.commandName}"`);
-        }
-        const command = interaction.client.commands.get(interaction.commandName);
-        if (!command) {
-            console.error(`No command matching ${interaction.commandName} was found.`);
-        }
-        if (!interaction.isStringSelectMenu()) {
-            try {
-                await command.execute(interaction);
+
+            const command = interaction.client.commands.get(interaction.commandName);
+            if (!command) {
+                console.error(`No command matching ${interaction.commandName} was found.`);
             }
-            catch (error) {
-                console.error(`Error executing ${interaction.commandName}`);
-                console.error(error);
+            if (!interaction.isStringSelectMenu()) {
+                try {
+                    await command.execute(interaction);
+                }
+                catch (error) {
+                    console.error(`Error executing ${interaction.commandName}`);
+                    console.error(error);
+                }
             }
         }
         if (interaction.isStringSelectMenu()) {
             for (const id of interaction.values) {
                 await interaction.member.roles.add(id);
             }
-        await interaction.update(`${interaction.member}'s Role has been updated`);
+            await interaction.update(`${interaction.member}'s Role has been updated`);
         }
     },
 };
