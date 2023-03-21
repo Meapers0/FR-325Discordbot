5aee51aaedcd29da0d97bf03767117cde0914821
Chase Condron <chasecondron@gmail.com>
2023-03-21 13:57:28 -0400

    Fixed bugs from merge

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,15 +1,16 @@
+const votedMembers = new Set();
+
 module.exports = {
     name: 'interactionCreate',
     once: false,
     async execute(interaction) {
-        if (!interaction.isCommand() || !interaction.isStringSelectMenu() || !interaction.isContextMenu()) {
+        if (!interaction.isCommand() || !interaction.isStringSelectMenu() || !interaction.isContextMenu() || !interaction.isButton()) {
             console.log(`Command Interaction ran. "${interaction.commandName}"`);
-
             const command = interaction.client.commands.get(interaction.commandName);
             if (!command) {
                 console.error(`No command matching ${interaction.commandName} was found.`);
             }
-            if (!interaction.isStringSelectMenu()) {
+            if (interaction.isCommand()) {
                 try {
                     await command.execute(interaction);
                 }
@@ -30,13 +31,51 @@ module.exports = {
             for (const id of removed) {
                 await interaction.member.roles.remove(id.value);
             }
-
-
             for (const id of interaction.values) {
                 await interaction.member.roles.add(id);
             }
 
             await interaction.update({ content: 'Your role has been updated', ephemeral: true });
+        }
+        if (interaction.isButton()) {
+            const splittedArray = interaction.customId.split('-');
+            if (splittedArray[0] !== 'Poll') return;
+
+            if (votedMembers.has(`${interaction.user.id}-${interaction.message.id}`)) {
+                return interaction.reply({ content: 'You already voted.', ephemeral: true });
+            }
+            votedMembers.add(`${interaction.user.id}-${interaction.message.id}`);
+            const pollEmbed = interaction.message.embeds[0];
+            if (!pollEmbed) {
+                return interaction.reply({
+                content: 'unable to find poll',
+                ephemeral: true,
+            });
+            }
+
+            const yesField = pollEmbed.fields[0];
+            const noField = pollEmbed.fields[1];
+
+            const VoteCountedReply = 'Vote Counted.';
+
+            switch (splittedArray[1]) {
+                case 'Yes': {
+                    const newYesCount = parseInt(yesField.value) + 1;
+                    yesField.value = newYesCount;
+
+                    interaction.reply({ content: VoteCountedReply, ephemeral: true });
+                    interaction.message.edit({ embeds: [pollEmbed] });
+                }
+                    break;
+                case 'No': {
+                    const newNoCount = parseInt(noField.value) + 1;
+                    noField.value = newNoCount;
+
+                    interaction.reply({ content: VoteCountedReply, ephemeral: true });
+                    interaction.message.edit({ embeds: [pollEmbed] });
+                }
+                    break;
+            }
         }
     },
 };
