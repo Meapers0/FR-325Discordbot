e3019af2ea8e10b9715c67f40d7e848505d2e1ed
mknox13 <114361859+mknox13@users.noreply.github.com>
2023-03-30 14:59:24 -0400

    Created stucommands folder and updated help command and index.js
    Index.js now looks for stucommands folder and help function is now split into admin and student commands. Poll.js was also moved into the stucommands folder

--- src/commands/help.js
+++ src/commands/help.js
@@ -4,13 +4,31 @@ module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('A list of commands')
+		.addSubcommand(subcommand =>
+			subcommand
+					.setName('admin')
+					.setDescription('Displays all commands'),
+					)
+					.setDefaultMemberPermissions(PermissionFlagsBits.Administrator)
+		.addSubcommand(subcommand =>
+			subcommand
+					.setName('student')
+					.setDescription('Displays all commands avaiable to students'))
 		.setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
 	async execute(interaction) {
 		try {
+			if (interaction.options.data[0].name === 'admin') {
 			const embed = new EmbedBuilder()
 				.addFields(
 					interaction.client.commandData);
 			interaction.reply({ content: 'Help!', ephemeral: true, embeds: [embed] });
+				}
+			if (interaction.options.data[0].name === 'student') {
+					const embed = new EmbedBuilder()
+						.addFields(
+							interaction.client.commandDataStu);
+					interaction.reply({ content: 'Help!', ephemeral: true, embeds: [embed] });
+						}
 		}
 		catch (error) {
 			console.error(error);
