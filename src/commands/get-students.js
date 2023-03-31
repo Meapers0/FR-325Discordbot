8f54f31d64791c3595704e3d0af9de7dd19630cb
tdeneweth427 <travdogsir@yahoo.com>
2023-03-30 20:34:19 -0400

    stats

---
+++ src/commands/get-students.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('get-students')
+        .setDescription('This will give you a count of the number of students in the discord')
+        .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+    async execute(interaction) {
+        const members = await interaction.guild.members.fetch();
+        const roles = await interaction.guild.roles.fetch();
+        let studentCount = 0;
+        let vetCount = 0;
+        for (const ele of members){
+            ele[1].roles.cache.forEach(async element => {
+                if (element.name.toLowerCase().includes('student')){
+                    studentCount++;
+                }
+                else if (element.name.toLowerCase().includes('veteran')){
+                    vetCount++;
+                }
+            });
+        }
+        await interaction.reply('The server has ' + studentCount + ' students and ' + vetCount + ' veterans.');
+    },
+};
