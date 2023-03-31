09cd2d3f3f50366d288d408d46aae0287e6d3f0d
Chase Condron <chasecondron@gmail.com>
2023-03-30 21:07:59 -0400

    Cleaned up

--- src/commands/get-students.js
+++ src/commands/get-students.js
@@ -4,18 +4,17 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('get-students')
         .setDescription('This will give you a count of the number of students in the discord')
-        .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages),
+        .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
     async execute(interaction) {
         const members = await interaction.guild.members.fetch();
-        const roles = await interaction.guild.roles.fetch();
         let studentCount = 0;
         let vetCount = 0;
-        for (const ele of members){
+        for (const ele of members) {
             ele[1].roles.cache.forEach(async element => {
-                if (element.name.toLowerCase().includes('student')){
+                if (element.name.toLowerCase().includes('student')) {
                     studentCount++;
                 }
-                else if (element.name.toLowerCase().includes('veteran')){
+                else if (element.name.toLowerCase().includes('veteran')) {
                     vetCount++;
                 }
             });
